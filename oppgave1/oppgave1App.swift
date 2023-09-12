998d974d89528aa1470b6a538c3bd153813133df
Zakaria <Zacke.berglund@outlook.com>
2023-09-12 14:45:40 +0200

    Initial Commit

---
+++ oppgave1/oppgave1App.swift
@@ -0,0 +1,17 @@
+//
+//  oppgave1App.swift
+//  oppgave1
+//
+//  Created by zakaria berglund on 12/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct oppgave1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
